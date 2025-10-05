6a1c2ff6a5ff042339eda2577ff4dd5fbf3d2b7e
jpSales <joaopedrodefaria>
2025-10-05 19:41:59 -0300

    chore: remove log

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { DashboardContent } from "@/components/dashboard-content";
 export default async function DashboardPage() {
   var categories = await getCategories();
   var topics = await getTopics();
-  console.log("TOPICS: ", topics);
   // Renderiza o Client Component e passa os dados para ele
 
   return <DashboardContent categories={categories} topics={topics} />;
